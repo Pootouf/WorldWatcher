3ffdc42f1b43dd7a18af51a00450db15187a2cef
Etienne Binginot <etienne.binginot@gmail.com>
2025-05-17 22:11:43 +0200

    feat: add surface temperature map notification

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 load_dotenv()
-surface_temperature_channel: int | None = os.getenv("CHANNEL_SURFACE_TEMPERATURE")
+surface_temperature_channel: int | None = int(os.getenv("CHANNEL_SURFACE_TEMPERATURE"))
+surface_temperature_map_channel: int | None = int(os.getenv("CHANNEL_SURFACE_TEMPERATURE_MAP"))
 
 
 async def wait_until(hour: int, minute: int = 0):
@@ -22,20 +23,29 @@ async def wait_until(hour: int, minute: int = 0):
 
 @client.event
 async def on_ready():
-    await wait_until(8)
+    #await wait_until(8)
     update_surface_temperature.start()
+    update_surface_temperature_map.start()
 
 
 @tasks.loop(hours=24)
 async def update_surface_temperature():
     if surface_temperature_channel is None:
         return
-
     channel = client.get_channel(surface_temperature_channel)
     date_str = (datetime.now() - timedelta(days=4)).strftime("%Y-%m-%d")
     link = f"https://sites.ecmwf.int/data/climatepulse/timeseries/era5_daily_series_2t_global_{date_str}.png"
-
     await channel.send(f"Graphique des températures de surface mise à jour: {link}")
+
+
+@tasks.loop(hours=24)
+async def update_surface_temperature_map():
+    if surface_temperature_map_channel is None:
+        return
+    channel = client.get_channel(surface_temperature_map_channel)
+    date_str = (datetime.now() - timedelta(days=4)).strftime("%Y%m%d")
+    link = f"https://sites.ecmwf.int/data/climatepulse/maps/download/daily/2t/anomaly/2025/climpulse_map_era5_download_daily_2t_anomaly_{date_str}.png"
+    await channel.send(f"Map des températures de surface mise à jour: {link}")
 
 
 TOKEN = os.getenv("DISCORD_TOKEN")
