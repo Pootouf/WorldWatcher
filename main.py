752cb00f28d80db8212ad24c6b85b14e3536b003
Etienne Binginot <etienne.binginot@gmail.com>
2025-05-17 21:56:58 +0200

    feat: add surface temperature notification

---
+++ main.py
@@ -0,0 +1,42 @@
+import asyncio
+from datetime import datetime, timedelta
+from discord.ext import tasks
+import discord
+from dotenv import load_dotenv
+import os
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+load_dotenv()
+surface_temperature_channel: int | None = os.getenv("CHANNEL_SURFACE_TEMPERATURE")
+
+
+async def wait_until(hour: int, minute: int = 0):
+    now = datetime.now()
+    target = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
+    if now >= target:
+        target += timedelta(days=1)
+    await asyncio.sleep((target - now).total_seconds())
+
+
+@client.event
+async def on_ready():
+    await wait_until(8)
+    update_surface_temperature.start()
+
+
+@tasks.loop(hours=24)
+async def update_surface_temperature():
+    if surface_temperature_channel is None:
+        return
+
+    channel = client.get_channel(surface_temperature_channel)
+    date_str = (datetime.now() - timedelta(days=4)).strftime("%Y-%m-%d")
+    link = f"https://sites.ecmwf.int/data/climatepulse/timeseries/era5_daily_series_2t_global_{date_str}.png"
+
+    await channel.send(f"Graphique des températures de surface mise à jour: {link}")
+
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+client.run(TOKEN)
