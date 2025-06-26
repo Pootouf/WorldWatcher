b1d3dafeb3e80cb60625aca9b217f326c8abbc5e
Etienne Binginot <etienne.binginot@gmail.com>
2025-06-26 20:40:50 +0200

    fix: fix some selenium options

--- main.py
+++ main.py
@@ -82,9 +82,12 @@ async def send_image_or_link(channel, link, message, filename):
 
 async def fetch_img_src_with_selenium(url, css_selector):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     driver.get(url)
     await asyncio.sleep(5)
